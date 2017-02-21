8a8d93260e6c164844aa2a86c83a399e185ed3bd
Angel Anichin <angel.anichin@markit.com>
2017-02-21 10:55:26 -0700

    temporary check-in

--- MyCodeCamp/Controllers/CampsController.cs
+++ MyCodeCamp/Controllers/CampsController.cs
@@ -1,58 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyCodeCamp.Data;
 using MyCodeCamp.Data.Entities;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace MyCodeCamp.Controllers
 {
-  [Route("api/[controller]")]
-  public class CampsController : Controller
-  {
-    private ICampRepository _repo;
-
-    public CampsController(ICampRepository repo)
+    [Route("api/[controller]")]
+    public class CampsController : Controller
     {
-      _repo = repo;
+        private ILogger<CampsController> _logger;
+        private ICampRepository _repo;
+
+        public CampsController(ICampRepository repo, ILogger<CampsController> logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            var camps = _repo.GetAllCamps();
+
+            return Ok(camps);
+        }
+
+        [HttpGet("{id}", Name = "CampGet")]
+        public IActionResult Get(int id, bool includeSpeakers = false)
+        {
+            try
+            {
+                Camp camp = null;
+
+                if (includeSpeakers) camp = _repo.GetCampWithSpeakers(id);
+                else camp = _repo.GetCamp(id);
+
+                if (camp == null) return NotFound($"Camp {id} was not found");
+
+                return Ok(camp);
+            }
+            catch
+            {
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Camp model)
+        {
+            try
+            {
+                _logger.LogInformation("Creating a new Code Camp");
+
+                _repo.Add(model);
+                if (await _repo.SaveAllAsync()){
+                    var newUri = Url.Link("CampGet", new { id = model.Id });
+                    return Created(newUri, model);
+                }else
+                {
+                    _logger.LogWarning("Could not save Camp to the database");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Threw exception while saving Camp: {ex}");
+            }
+
+            return BadRequest();
+        }
+
     }
-
-    [HttpGet("")]
-    public IActionResult Get()
-    {
-      var camps = _repo.GetAllCamps();
-
-      return Ok(camps);
-    }
-
-    [HttpGet("{id}")]
-    public IActionResult Get(int id, bool includeSpeakers = false)
-    {
-      try
-      {
-        Camp camp = null;
-
-        if (includeSpeakers) camp = _repo.GetCampWithSpeakers(id);
-        else camp = _repo.GetCamp(id);
-
-        if (camp == null) return NotFound($"Camp {id} was not found");
-
-        return Ok(camp);
-      }
-      catch
-      {
-      }
-
-      return BadRequest();
-    }
-
-  }
 }
-
-
-
-
-
-
