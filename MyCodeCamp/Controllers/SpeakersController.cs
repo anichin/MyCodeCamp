dfa59f30f8fe84dd8a24b570e40bb689a4bc79c2
Angel Anichin <anichin@gmail.com>
2017-02-22 11:43:33 -0700

    completed "Demo: Association GET"

---
+++ MyCodeCamp/Controllers/SpeakersController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyCodeCamp.Data;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyCodeCamp.Controllers
+{
+    [Route("api/camps/{moniker}/speakers")]
+    public class SpeakersController : BaseController
+    {
+        private ICampRepository _repository;
+        private ILogger<SpeakersController> _logger;
+        private IMapper _mapper;
+
+        public SpeakersController(ICampRepository repository
+            ,ILogger<SpeakersController> logger,
+            IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string moniker)
+        {
+            var speakers = _repository.GetSpeakersByMoniker(moniker);
+
+            return Ok(speakers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string moniker, int id)
+        {
+            var speaker = _repository.GetSpeaker(id);
+
+            if (speaker == null)
+            {
+                return NotFound();
+            }
+
+            if (speaker.Camp.Moniker != moniker)
+            {
+                return BadRequest("Speaker not in specified camp");
+            }
+
+            return Ok(speaker);
+        }
+    }
+}
