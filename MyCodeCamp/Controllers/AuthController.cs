0f3c84951810eb1ab6c353f6cfa94827e04711c4
Angel Anichin <angel.anichin@markit.com>
2017-03-06 14:33:14 -0700

    completed "Demo: Using Claims"

--- MyCodeCamp/Controllers/AuthController.cs
+++ MyCodeCamp/Controllers/AuthController.cs
@@ -74,11 +74,15 @@ namespace MyCodeCamp.Controllers
                 {
                     if (_hasher.VerifyHashedPassword(user, user.PasswordHash, model.Password) == PasswordVerificationResult.Success)
                     {
+                        var userClaims = await _userMgr.GetClaimsAsync(user);
                         var claims = new[]
                         {
                             new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
                             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                        };
+                            new Claim(JwtRegisteredClaimNames.GivenName, user.FirstName),
+                            new Claim(JwtRegisteredClaimNames.FamilyName, user.LastName),
+                            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                        }.Union(userClaims);
 
                         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
                         var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
